d8f401b2fd2ffc8660f41f80dcc3b55163c3d655
Yamanabe <68895077+Yamanabe@users.noreply.github.com>
2022-12-13 21:05:08 +0900

    1213

---
+++ tkzw/app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\News; // News Modelを使う
+use App\History;
+
+use Carbon\Carbon;
+
+class NewsController extends Controller
+{
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, News::$rules); // Validationを行う
+
+        $news = new News;
+        $form = $request->all();
+
+        // フォームから画像が送信されたら保存をして$news->image_pathに画像のパスを保存
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+
+        // フォームから送信されてきた_token, imageを削除
+        unset($form['_token']);
+        unset($form['image']);
+
+        // データベースに保存
+        $news->fill($form);
+        $news->save();
+
+        return redirect('admin/news/create');
+    }
+
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = News::where('title', $cond_title)->get();
+        } else {
+            $posts = News::all();
+        }
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
+    public function edit(Request $request)
+    {
+        $news = News::find($request->id);
+        if (empty($news)) {
+            abort(404);
+        }
+        return view('admin.news.edit', ['news_form' => $news]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        $news = News::find($request->input('id'));
+        $news_form = $request->all();
+        if ($request->input('remove')) {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+        // $news = News::find($request->id);
+        // $news_form = $request->all();
+        // if (isset($news_form['image'])) {
+        //     $path = $request->file('image')->store('public/image');
+        //     $news->image_path = basename($path);
+        //     unset($news_form['image']);
+        // } elseif (0 == strcmp($request->remove, 'true')) {
+        //     $news->image_path = null;
+        // }
+        unset($news_form['_token']);
+        unset($news_form['remove']);
+        unset($news_form['image']);
+
+        $news->fill($news_form)->save();
+
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+        return redirect('admin/news');
+    }
+
+    public function delete(Request $request)
+    {
+        $news = News::find($request->id);
+        $news->delete();
+        return redirect('admin/news/');
+    }
+}
